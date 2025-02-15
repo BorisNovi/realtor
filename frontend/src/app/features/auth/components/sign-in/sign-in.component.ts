903d1b63db20e60df77d36ed4e491c87cfc5d540
BorisNovi <borisnovikov4815@gmail.com>
2025-02-15 21:43:30 +0400

    feat: add sign-in form

--- frontend/src/app/features/auth/components/sign-in/sign-in.component.ts
+++ frontend/src/app/features/auth/components/sign-in/sign-in.component.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SignInComponent {}
+export class SignInComponent {
+  private fb = inject(FormBuilder);
+
+  public form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/),
+      ],
+    ],
+  });
+
+  public onSubmit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(this.form.value);
+  }
+}
