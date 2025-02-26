670b1addbf1d6edea8b241315f54ac8538e4c8b9
Dmitrii Glukhov <dm.gluhovv@gmail.com>
2025-02-27 01:52:11 +0400

    fix: correct paths

--- backend/user_auth/urls.py
+++ backend/user_auth/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),  # эндпоинт для регистрации
-    path('api/signin/', views.SigninView.as_view(), name='signin'), # эндпоинт для входа
+    path('signup/', views.signup, name='signup'),  # api/v1/auth/signup/
+    path('signin/', views.SigninView.as_view(), name='signin'),  # api/v1/auth/signin/
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
