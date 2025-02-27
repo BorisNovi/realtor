aeb9d4343f7b0b1afb9042fe1cac4473270b129f
Dmitrii Glukhov <dm.gluhovv@gmail.com>
2025-02-28 01:45:55 +0400

    feats: recover + recover-activate

--- backend/user_auth/urls.py
+++ backend/user_auth/urls.py
@@ -2,12 +2,13 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
  
 from django.urls import path
 from . import views
-from .views import LogoutView, ProtectedView
+from .views import LogoutView, PasswordRecoveryView, PasswordResetActivateView, ProtectedView
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),  # api/v1/auth/signup/
     path('signin/', views.SigninView.as_view(), name='signin'),  # api/v1/auth/signin/
     path('logout/', LogoutView.as_view(), name='logout'),
     path('protected-endpoint/', ProtectedView.as_view(), name='protected-endpoint'),
-
+    path('recover/', PasswordRecoveryView.as_view(), name='password-recover'),
+    path('recover-activate/', PasswordResetActivateView.as_view(), name='password-reset-activate'),
 ]
