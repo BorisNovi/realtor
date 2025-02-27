56fa283c7e127fda237e219edc340726e3e50078
Dmitrii Glukhov <dm.gluhovv@gmail.com>
2025-02-28 00:56:23 +0400

    feat: protected-endpoint

--- backend/user_auth/views.py
+++ backend/user_auth/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 
 from .serializers import SignupSerializer
@@ -23,3 +25,21 @@ class SigninView(APIView):
         if serializer.is_valid():
             return Response(serializer.validated_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "Logout successful"}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response({"error": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
+        
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]  # Требует аутентификации
+
+    def get(self, request):
+        return Response({"message": "Я смотрю у тебя есть токен. Добро пожаловать, милсдарь"}, status=200)        
