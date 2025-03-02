022b830882e60c6a6698285847f95e087ab94ad4
Dmitrii Glukhov <dm.gluhovv@gmail.com>
2025-03-03 01:11:13 +0400

    fix: sign-up with Redis + sign-up-activate (response = ISessionUser)

--- backend/user_auth/urls.py
+++ backend/user_auth/urls.py
@@ -5,10 +5,14 @@ from . import views
 from .views import LogoutView, PasswordRecoveryView, PasswordResetActivateView, ProtectedView
 
 urlpatterns = [
-    path('sign-up/', views.signup, name='signup'),  
+    path('sign-up/', views.signup, name='signup'),
+    path('sign-up-activate/', views.signup_activate, name='sign-up-activate'),  
+    
     path('sign-in/', views.SigninView.as_view(), name='signin'),  
+    
     path('logout/', LogoutView.as_view(), name='logout'),
     path('protected-endpoint/', ProtectedView.as_view(), name='protected-endpoint'),
+    
     path('recover/', PasswordRecoveryView.as_view(), name='password-recover'),
     path('recover-activate/', PasswordResetActivateView.as_view(), name='password-reset-activate'),
 ]
