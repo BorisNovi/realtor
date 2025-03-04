2f35aa88d366e450cc8c6448d2e94a364b6ee0df
BorisNovi <borisnovikov4815@gmail.com>
2025-03-05 01:32:26 +0400

    feat: add profile forms and logout

--- frontend/src/app/features/profile/profile.component.ts
+++ frontend/src/app/features/profile/profile.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FluidModule } from 'primeng/fluid';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { DividerModule } from 'primeng/divider';
+import { Store } from '@ngxs/store';
+import { Logout } from 'src/app/core';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [InputTextModule, FluidModule, ButtonModule, FormsModule, DividerModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  private store = inject(Store);
+
+  logOut(): void {
+    this.store.dispatch(new Logout());
+  }
 }
