01d678780e35d231bebe356e68f1c9e06c74d3b7
Boris Novikov <borisnovikov4815@gmail.com>
2025-03-16 22:41:26 +0400

    feat: add selectors

--- frontend/src/app/core/catalog/state/catalog.state.ts
+++ frontend/src/app/core/catalog/state/catalog.state.ts
@@ -1,5 +1,7 @@
-import { State } from '@ngxs/store';
+import { inject, Injectable } from '@angular/core';
+import { Selector, State } from '@ngxs/store';
 import { ICatalogItem, IPagination, ITableData } from '@shared/interfaces';
+import { CatalogService } from '../shared';
 
 interface CatalogStateModel {
   catalog: ITableData<ICatalogItem>;
@@ -7,14 +9,34 @@ interface CatalogStateModel {
   pagination: IPagination;
 }
 
-// @State<CatalogStateModel>({
-//   name: 'catalog',
-//   defaults: {
-//     catalog: { items: [], total: 0 },
-//     pagination: {
-//       pageIndex: 0,
-//       pageSize: 20
-//     },
-//     loading: false,
-//   },
-// })
+@State<CatalogStateModel>({
+  name: 'catalog',
+  defaults: {
+    catalog: { items: [], total: 0 },
+    pagination: {
+      pageIndex: 0,
+      pageSize: 20,
+    },
+    loading: false,
+  },
+})
+@Injectable()
+export class CatalogState {
+  private readonly catalogService = inject(CatalogService);
+
+  // Selectors
+  @Selector()
+  public static loading({ loading }: CatalogStateModel) {
+    return loading;
+  }
+
+  @Selector()
+  public static offices({ catalog }: CatalogStateModel) {
+    return catalog;
+  }
+
+  @Selector()
+  public static pagination({ pagination }: CatalogStateModel) {
+    return pagination;
+  }
+}
