415dec463692851fba10e3c4698318042827908f
writervox <dm.gluhovv@gmail.com>
2025-04-06 13:59:26 +0400

    feat: session check

--- backend/user_auth/urls.py
+++ backend/user_auth/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from user_auth import views
 from .views.activation import PasswordResetActivateView
 from .views.auth import LogoutView
+from .views.auth import check_session
 from .views.password import PasswordRecoveryView
 from .views.refresh import RefreshTokenView
+
 
 urlpatterns = [
     path('sign-up', views.signup, name='signup'),
@@ -13,4 +15,5 @@ urlpatterns = [
     path('recover', PasswordRecoveryView.as_view(), name='password-recover'),
     path('recover-activate', PasswordResetActivateView.as_view(), name='password-reset-activate'),
     path('refresh', RefreshTokenView.as_view(), name='refresh_token'),
+    path('sessions/check', check_session, name='check_session'),
 ]
