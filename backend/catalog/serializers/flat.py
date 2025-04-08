97dbbcb8d43f015b25043403ec6e65e630d89b4d
writervox <dm.gluhovv@gmail.com>
2025-04-09 01:30:25 +0400

    feat: catalog

---
+++ backend/catalog/serializers/flat.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from catalog.models import Flat
+
+class FlatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flat
+        fields = '__all__'
+        extra_kwargs = {
+            'rooms': {'required': False},
+            'floor_current': {'required': False},
+            'floor_full': {'required': False},
+            'bath': {'required': False},
+            'shower': {'required': False},
+            'air_conditioning': {'required': False},
+            'fireplace': {'required': False},
+            'beautiful_view': {'required': False},
+            'new_building': {'required': False},
+            'elevator': {'required': False},
+        }
--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .serializers.catalog import CatalogCreateSerializer
 
-# Create your views here.
+class PropertyCreateView(generics.CreateAPIView):
+    serializer_class = CatalogCreateSerializer
+    #permission_classes = [permissions.IsAuthenticated]
+    
+    def perform_create(self, serializer):
+        # Выводим данные, которые приходят в запросе
+        print("Received data:", self.request.data)
+
+        # Теперь вызываем сохранение объекта с помощью сериализатора
+        serializer.save()
