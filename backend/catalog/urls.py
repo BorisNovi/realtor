323bda0bac59c3651c526b96b3563f3585938c42
writervox <dm.gluhovv@gmail.com>
2025-04-15 00:15:54 +0400

    fix

--- backend/catalog/urls.py
+++ backend/catalog/urls.py
@@ -3,5 +3,5 @@ from .views import PropertyCreateView
 
 urlpatterns = [
     path('/property_object', PropertyCreateView.as_view(), name='property-create'),
-    path('/catalog')
+    # path('/catalog')
 ]
