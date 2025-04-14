fe7a0c224c2047225a48fb705534bda982b27aec
writervox <dm.gluhovv@gmail.com>
2025-04-15 00:15:27 +0400

    feat: camelCase renderer

--- backend/catalog/urls.py
+++ backend/catalog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import PropertyCreateView
 
 urlpatterns = [
-    path('', PropertyCreateView.as_view(), name='property-create'),
+    path('/property_object', PropertyCreateView.as_view(), name='property-create'),
+    path('/catalog')
 ]
--- backend/realtor/settings.py
+++ backend/realtor/settings.py
@@ -80,6 +80,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+
+    'DEFAULT_RENDERER_CLASSES': [
+        'realtor.utils.renderers.CamelCaseJSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+
 }
 
 SIMPLE_JWT = {
