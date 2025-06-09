239dd15e906b88b85086eb2678f84be708fb7f90
writervox <dm.gluhovv@gmail.com>
2025-06-09 23:44:54 +0400

    feat: DETAILS

--- backend/catalog/urls.py
+++ backend/catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from catalog.views.catalog_detail_view import CatalogDetailView
 from catalog.views.property_create_view import PropertyCreateView
 from catalog.views.catalog_list_view import CatalogListView
 from catalog.views.catalog_bulk_delete_view import CatalogBulkDeleteView
@@ -7,5 +8,6 @@ urlpatterns = [
     path('/property_object', PropertyCreateView.as_view(), name='property-create'),
     path('/catalog', CatalogListView.as_view(), name='catalog'),
     path('/catalog/delete', CatalogBulkDeleteView.as_view(), name="catalog-bulk-delete"),
+    path('/catalog/<int:pk>', CatalogDetailView.as_view(), name='catalog-detail'),
 
 ]
