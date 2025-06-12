17b55842a025d312881c89d909fefac7f8fd2c84
Boris Novikov <borisnovikov4815@gmail.com>
2025-06-12 20:53:46 +0400

    feat: add info to deatil, make rows clickable

--- frontend/src/app/shared/components/detail/detail.component.ts
+++ frontend/src/app/shared/components/detail/detail.component.ts
@@ -14,8 +14,8 @@ export class DetailComponent {
   readonly propertyObject = input<IPropertyObject>();
 
   images: any[] = [
-    { image: 'https://picsum.photos/id/237/1200/1000', thumbnail: 'https://picsum.photos/id/237/100/100' },
     { image: 'https://picsum.photos/id/238/1200/1000', thumbnail: 'https://picsum.photos/id/238/100/100' },
+    { image: 'https://picsum.photos/id/237/1200/1000', thumbnail: 'https://picsum.photos/id/237/100/100' },
     { image: 'https://picsum.photos/id/239/1200/1000', thumbnail: 'https://picsum.photos/id/239/100/100' },
     { image: 'https://picsum.photos/id/240/1200/1000', thumbnail: 'https://picsum.photos/id/240/100/100' },
     { image: 'https://picsum.photos/id/241/1200/1000', thumbnail: 'https://picsum.photos/id/241/100/100' },
