a40657619d6638a80e3f6a33dc2b664d9949c92b
Boris Novikov <borisnovikov4815@gmail.com>
2025-06-15 00:13:00 +0400

    feat: add map markers

--- frontend/src/app/features/map/map-page.component.ts
+++ frontend/src/app/features/map/map-page.component.ts
@@ -1,16 +1,17 @@
 import { Component, signal, viewChild } from '@angular/core';
 import { MapComponent } from '@shared/components';
+import { MapMarkerComponent } from '@shared/components/map/map-marker.component';
 import { LngLatLike } from 'maplibre-gl';
 
 @Component({
   selector: 'app-map-page',
-  imports: [MapComponent],
+  imports: [MapComponent, MapMarkerComponent],
   templateUrl: './map-page.component.html',
 })
 export class MapPageComponent {
   readonly mapComponent = viewChild(MapComponent);
 
-  readonly c = signal<LngLatLike>([0, 0]);
+  readonly c = signal<LngLatLike>([41, 41]);
 
   // XXX: Здесь пока все тестовое, оно не пойдет в прод
   on1(): void {
@@ -20,5 +21,9 @@ export class MapPageComponent {
 
   on2(): void {
     this.c.set([16, 11]);
+  }
+
+  on3(data: any): void {
+    console.log(data);
   }
 }
