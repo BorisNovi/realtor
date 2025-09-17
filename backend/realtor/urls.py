b94c80f832bc8f971af8dc411b775d4d8806b2cb
Dmitrii Glukhov <dm.gluhovv@gmail.com>
2025-09-17 21:49:22 +0400

    fix: contact routing path

--- backend/realtor/urls.py
+++ backend/realtor/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('user_auth.urls')),
     path('api/v1/', include('catalog.urls')),
-    path('api/v1/contacts/', include('contacts.urls')),
+    path('api/v1/contact/', include('contacts.urls')), # Убрал С на конце
 ]
