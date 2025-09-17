21a1f48b3941cbad954fb93bd38ef5f884ef683c
Dmitrii Glukhov <dm.gluhovv@gmail.com>
2025-09-17 22:15:57 +0400

    feat: contact upgrade

--- backend/contacts/urls.py
+++ backend/contacts/urls.py
@@ -2,13 +2,7 @@ from django.urls import path
 from .views import ContactView
 
 urlpatterns = [
-    # path('', ContactView.as_view(), name='create_contact'), # Список и создание контакта
-    # path('<int:pk>/', ContactView.as_view(), name='update_contact'), # Обновление конкретного контакта
-
-# новые пути. Убрал С на конце. 
     path('list/', ContactView.as_view(), name='contact-list'),             # список контактов
     path('', ContactView.as_view(), name='contact-create'),                # создание контакта
     path('<int:pk>/', ContactView.as_view(), name='contact-detail'),       # получение, обновление, удаление конкретного контакта
-
-
 ]
