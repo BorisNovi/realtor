5bf1edb95eb5529b339f70690fccd27ea8a9185c
BorisNovi <borisnovikov4815@gmail.com>
2025-10-27 01:24:39 +0400

    feat: add animated bg for auth pages

--- frontend/src/app/shared/directives/index.ts
+++ frontend/src/app/shared/directives/index.ts
@@ -1,3 +1,4 @@
+export { AnimatedBgDirective } from './animated-bg.directive';
 export { IconDirective } from './icon.directive';
 export { ScrollToTopOnShowDirective } from './scroll-to-on-show.directive';
 export { WorldPhoneMasksDirective } from './world-phone-mask.directive';
