c7491043a9e63b95b0ad571198b5867c50326ee4
BorisNovi <borisnovikov4815@gmail.com>
2025-10-26 16:01:22 +0400

    feat/docs: add listings interface, add comments to auth interceptor

---
+++ frontend/src/app/shared/interfaces/listing.interface.ts
@@ -0,0 +1,16 @@
+import { IPropertyObject } from "./property-object.interface";
+
+export interface IListing {
+  id: number;
+  dateAdded: string | Date;
+  name: string;
+  propertyObjects: IPropertyObject[];
+  publicLink: IPublicLink;
+  companyName?: string;
+  companyLogo?: string; // imageURL
+}
+
+interface IPublicLink {
+  url: string;
+  available: boolean;
+}
