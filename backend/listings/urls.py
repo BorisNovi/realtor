d0abcf63f1e671f580186b3add70a259a21e7cfd
Dmitrii Glukhov <dm.gluhovv@gmail.com>
2025-12-02 14:52:01 +0400

    Фича: завершена работа по настройке доступов к подборкам по разным ссылкам.

--- backend/listings/urls.py
+++ backend/listings/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import listing_views
 
 urlpatterns = [
-    path('', listing_views.ListingsView.as_view(), name='listing-create'),                # создание листинга
-    path('/list', listing_views.ListingsView.as_view(), name='listing-list'),             # список листингов
-    path('/<int:pk>', listing_views.ListingsView.as_view(), name='listing-detail'),       # получение, обновление, удаление листинга
+    path('', listing_views.ListingsView.as_view(), name='listing-create'),                        # создание листинга
+    path('/<int:pk>', listing_views.ListingsView.as_view(), name='listing-detail'),               # получение, обновление, удаление листинга
+    path('/<str:token>', listing_views.PublicListingRedirectView.as_view(), name="public-listing"),  # получение 1 открытого листинга
+    path('/list', listing_views.ListingsView.as_view(), name='listing-list'),                     # список листингов
 ]
+
+# ВАЖНО: порядок путей имеет значение!
