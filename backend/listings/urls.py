137d140af8fd9d85353ab59fefae108532966650
Dmitrii Glukhov <dm.gluhovv@gmail.com>
2025-12-02 18:49:51 +0400

    Фикс: порядок путей исправлен, список подборок отдается

--- backend/listings/urls.py
+++ backend/listings/urls.py
@@ -4,9 +4,9 @@ from . import listing_views
 
 urlpatterns = [
     path('', listing_views.ListingsView.as_view(), name='listing-create'),                        # создание листинга
+    path('/list', listing_views.ListingsView.as_view(), name='listing-list'),                     # список листингов
     path('/<int:pk>', listing_views.ListingsView.as_view(), name='listing-detail'),               # получение, обновление, удаление листинга
     path('/<str:token>', listing_views.PublicListingRedirectView.as_view(), name="public-listing"),  # получение 1 открытого листинга
-    path('/list', listing_views.ListingsView.as_view(), name='listing-list'),                     # список листингов
 ]
 
 # ВАЖНО: порядок путей имеет значение!
