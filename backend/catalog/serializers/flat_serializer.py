1f7e325a2575c43842e10953884fb82f44538517
Dmitrii Glukhov <dm.gluhovv@gmail.com>
2025-12-04 13:12:12 +0400

    Фикс: shared facilities

--- backend/catalog/serializers/flat_serializer.py
+++ backend/catalog/serializers/flat_serializer.py
@@ -8,6 +8,7 @@ class FlatSerializer(serializers.ModelSerializer):
         model = Flat
         fields = '__all__'
 
+    # Отдаем фронту объект в нужном формате
     def to_representation(self, instance):
         data = super().to_representation(instance)
 
@@ -48,7 +49,7 @@ class FlatSerializer(serializers.ModelSerializer):
                 "kitchen": data.get("kitchen_type"),
                 "options": {
                     "sharedFacilities": {
-                        "kitchen": data.get("shared_kitchen"),
+                        "kitchen": data.get("shared_kitchen"), # Здесь ОК - отдает в нужном виде. 
                         "bathroom": data.get("shared_bathroom"),
                     },
                     "utilities": {
@@ -74,3 +75,47 @@ class FlatSerializer(serializers.ModelSerializer):
                 },
             },
         }
+    
+# Вспомогательная функция для подготовки данных из specifics для создания/обновления в общем сериализаторе.
+def prepare_property_data(validated_data):
+    """Возвращает распарсенные поля, готовые к применению к модели"""
+    specifics = validated_data.pop('specifics', {}) or {}
+
+    options = specifics.get("options", {}) or {}
+    shared_facilities = options.get("shared_facilities", {}) or {}
+    utilities = options.get("utilities", {}) or {}
+    other = options.get("other", {}) or {}
+
+    # Общие поля
+    data = {
+        "rooms": specifics.get("rooms"),
+        "floor_current": specifics.get("floor", {}).get("current"),
+        "floor_full": specifics.get("floor", {}).get("full"),
+        "kitchen_type": specifics.get("kitchen"),
+        "heating": specifics.get("heating"),
+        "furnished": specifics.get("furnished"),
+        "renovation": specifics.get("renovation"),
+        # Shared Facilities
+        "shared_kitchen": shared_facilities.get("kitchen", False),
+        "shared_bathroom": shared_facilities.get("bathroom", False),
+        # Utilities
+        "electricity": utilities.get("electricity", False),
+        "water_supply": utilities.get("water_supply", False),
+        "natural_gas": utilities.get("natural_gas", False),
+        "sewerage": utilities.get("sewerage", False),
+        "internet": utilities.get("internet", False),
+        # Other
+        "bath": other.get("bath", False),
+        "shower": other.get("shower", False),
+        "air_conditioning": other.get("air_conditioning", False),
+        "fireplace": other.get("fireplace", False),
+        "beautiful_view": other.get("beautiful_view", False),
+        "new_building": other.get("new_building", False),
+        "elevator": other.get("elevator", False),
+        "parking": other.get("parking", False),
+        "balcony": other.get("balcony", False),
+        "garden": other.get("garden", False),
+        "garage": other.get("garage", False),
+    }
+
+    return data
