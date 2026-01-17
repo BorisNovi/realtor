eb886b04a8bbcb8e118156ba96eeac972bed0893
BorisNovi <borisnovikov4815@gmail.com>
2026-01-17 16:47:59 +0400

    feat: add info to sign-up

--- frontend/src/app/shared/components/index.ts
+++ frontend/src/app/shared/components/index.ts
@@ -1,6 +1,7 @@
 export * from './address-picker/address-picker.component';
 export * from './detail/detail.component';
 export * from './fieldset-checkbox-group/fieldset-checkbox-group.component';
+export * from './info-block/info-block.component';
 export * from './input-wrapper/input-wrapper.component';
 export * from './link-switch/link-switch.component';
 export * from './map/map.component';
