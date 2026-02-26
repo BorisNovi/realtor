2a49252b9b4dac40ef20a318dab56e622e29f36f
Dmitrii Glukhov <dm.gluhovv@gmail.com>
2026-02-26 13:13:40 +0400

    Фича: стриминг-экспорт без специфик

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .user_views import ProfileView, ChangePasswordView, DeleteProfileView
+from .user_views import ProfileView, ChangePasswordView, DeleteProfileView, export_csv_view
 
 urlpatterns = [
     path('', ProfileView.as_view(), name='profile'),
     path('/change-password', ChangePasswordView.as_view(), name='change-password'),
     path('/delete', DeleteProfileView.as_view(), name='delete-profile'),
+    path('/export', export_csv_view, name='export-csv'),
 ]
 
 # проверено, все работает. слэши в начале URLов нужны!
